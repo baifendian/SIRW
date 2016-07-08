69e48cfcdfc80d59c1876f90d92dd46efd1f6b7e
tongtie <tie0214@126.com>
2016-07-08 18:23:35 +0800

    fix bug

--- wechat_service/urls.py
+++ wechat_service/urls.py
@@ -6,5 +6,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^$', views.wechat_index),
     url(r'^set/record/$', views.set_stock_record, name='set_record'),
 ]
