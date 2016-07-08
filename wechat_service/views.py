77aadd3a83bd2e393f8874c1489ca5980e798a56
tongtie <tie0214@126.com>
2016-07-08 18:09:01 +0800

    print error

--- wechat_service/views.py
+++ wechat_service/views.py
@@ -1,4 +1,5 @@
 # -*-coding:utf-8-*-
+import sys
 from django.http.response import HttpResponse, HttpResponseBadRequest
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
@@ -21,36 +22,40 @@ wechat = WechatBasic(conf=conf)
 
 @csrf_exempt
 def wechat_index(request):
-    print request
-    if request.method == 'GET':
-        signature = request.GET.get('signature')
-        timestamp = request.GET.get('timestamp')
-        nonce = request.GET.get('nonce')
-        if not wechat.check_signature(signature=signature, timestamp=timestamp, nonce=nonce):
-            return HttpResponseBadRequest('Verify Failed')
-        return HttpResponse(request.GET.get('echostr', ''), content_type="text/plain")
     try:
-        wechat.parse_data(data=request.body)
-    except ParseError:
-        return HttpResponseBadRequest('Invalid XML Data')
-    message = wechat.get_message()
-    response = wechat.response_text(content=FOLLOW_MESSAGE)
-    if isinstance(message, TextMessage):
-        content = message.content.strip().encode("utf-8")
-        reply_text_dict = {
-            "功能": FUNCTION_MESSAGE,
-           # "1": STOCK_MESSAGE,
-            "1": set_stock_record(),
-            "2": show_history(),
-            "3": show_help(),
-            "201": get_stock_history_info("510900"),
-            "202": get_stock_history_info("159920"),
-            "203": get_stock_history_info("510300"),
-            "204": get_stock_history_info("510500"),
-        }
-        reply_text = reply_text_dict.get(content, PROMPT_MESSAGE)
-        response = wechat.response_text(content=reply_text)
-    return HttpResponse(response, content_type="application/xml")
+        print request
+        if request.method == 'GET':
+            signature = request.GET.get('signature')
+            timestamp = request.GET.get('timestamp')
+            nonce = request.GET.get('nonce')
+            if not wechat.check_signature(signature=signature, timestamp=timestamp, nonce=nonce):
+                return HttpResponseBadRequest('Verify Failed')
+            return HttpResponse(request.GET.get('echostr', ''), content_type="text/plain")
+        try:
+            wechat.parse_data(data=request.body)
+        except ParseError:
+            return HttpResponseBadRequest('Invalid XML Data')
+        message = wechat.get_message()
+        response = wechat.response_text(content=FOLLOW_MESSAGE)
+        if isinstance(message, TextMessage):
+            content = message.content.strip().encode("utf-8")
+            reply_text_dict = {
+                "功能": FUNCTION_MESSAGE,
+               # "1": STOCK_MESSAGE,
+                "1": set_stock_record(),
+                "2": show_history(),
+                "3": show_help(),
+                "201": get_stock_history_info("510900"),
+                "202": get_stock_history_info("159920"),
+                "203": get_stock_history_info("510300"),
+                "204": get_stock_history_info("510500"),
+            }
+            reply_text = reply_text_dict.get(content, PROMPT_MESSAGE)
+            response = wechat.response_text(content=reply_text)
+        return HttpResponse(response, content_type="application/xml")
+    except Exception as e:
+        print sys.exc_info()
+        return []
 
 
 def show_help():
